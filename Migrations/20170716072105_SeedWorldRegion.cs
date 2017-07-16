09bb9f89457e5e61a5141f0ef359f50fa4671968
Andriy Arysmyatov <andriy.arysmyatov@outlook.com>
2017-07-16 10:43:31 +0300

    UI changes for the query results and it's 'world ' added as a region

---
+++ Migrations/20170716072105_SeedWorldRegion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NXS.Migrations
+{
+    public partial class SeedWorldRegion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Regions (Name, ParentRegionId) VALUES ('World', (SELECT ID FROM ParentRegions WHERE Name = 'EU'))");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
