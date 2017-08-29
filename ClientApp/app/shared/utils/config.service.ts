d5d2ebededf878e175d9b423e507ec1f1a120e5c
Andriy Arysmyatov <andriy.arysmyatov@outlook.com>
2017-08-29 12:02:18 +0300

    ConfigService  Changes for the production

--- ClientApp/app/shared/utils/config.service.ts
+++ ClientApp/app/shared/utils/config.service.ts
@@ -6,8 +6,8 @@ export class ConfigService {
     _apiURI : string;
  
     constructor() {
-        //this._apiURI = "http://theresourcenexus.co.uk/api";
-        this._apiURI = "http://localhost:5000/api";
+        this._apiURI = "http://theresourcenexus.co.uk/api";
+        //this._apiURI = "http://localhost:5000/api";
      }
  
      getApiURI() {
